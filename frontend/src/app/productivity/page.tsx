08bb6abe673e6ad032c11dcf308420ce8a62f54b
bchh325 <bbchhorb@gmail.com>
2025-03-22 12:27:45 -0700

    stuff

--- frontend/src/app/productivity/page.tsx
+++ frontend/src/app/productivity/page.tsx
@@ -1,8 +1,35 @@
 import React from "react";
 import styles from "./page.module.css"
 
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <div className={styles.header}>Header</div>
+        <div className={styles.content}>
+          <div>
+            <div className={styles.left}>
+              <div className={styles.header}>Pomodo Timer</div>
+              <div className={styles.timer}>Timer</div>
+              <div className={styles.breaks}>
+                <div>Button</div>
+                <div>Button</div>
+                <div>Button</div>
+              </div>
+              <div className={styles.startreset}>
+                <div>start</div>
+                <div>start</div>
+              </div>
+              <div>Sessions Completed</div>
+            </div>
+          </div>
+          <div>
+            <div className={styles.right}>Right content</div>
+          </div>
+        </div>
+      </div>
+    </div>);
 };
 
 export default page;
